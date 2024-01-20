1e1feebec1aad4af132f52c0666a8f9b00cc0de5
William <ko.william.robert@gmail.com>
2024-01-20 14:26:13 -0800

    Finished login

--- evie-client/src/pages/Login.js
+++ evie-client/src/pages/Login.js
@@ -1,9 +1,34 @@
 import React, { useState } from "react";
+import '../scss/_login.scss';
 
 function Login() {
+
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleLogin = () => {
+    // Add your login logic here
+    console.log(`Login with username: ${username} and password: ${password}`);
+  };
+
   return (
-    <div>
+    <div className="login-container">
       <h1>Login</h1>
+      <form className="login-form">
+        <label>
+          Username:
+          <input type="text" value={username} onChange={(e) => setUsername(e.target.value)} />
+        </label>
+        <br />
+        <label>
+          Password:
+          <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
+        </label>
+        <br />
+        <button type="button" onClick={handleLogin} className="login-button">
+          Login
+        </button>
+      </form>
     </div>
   );
 }
