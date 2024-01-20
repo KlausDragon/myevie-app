a95e2d7cb2a485e924ec8b8cbfd8215336755424
Ali Abbasi <ali_sitrick@yahoo.com>
2024-01-20 14:01:58 -0800

    removed index.css and added content to the landing page

--- evie-client/src/components/Header.js
+++ evie-client/src/components/Header.js
@@ -2,10 +2,6 @@ function Header() {
   return (
     <div className="Header">
       <h1>Header(img)</h1>
-      <button>
-        <a href="./login">Login</a>
-      </button>
-      <button>Signup</button>
     </div>
   );
 }
--- evie-client/src/pages/Landing.js
+++ evie-client/src/pages/Landing.js
@@ -1,11 +1,27 @@
 import React from "react";
-import Footer from "../components/Footer";
 
 function Landing() {
   return (
     <div>
       <h1>Welcome to Evie</h1>
-
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi
+        consectetur accusantium sed amet porro, est ipsa dicta? Iste accusamus
+        culpa, dolorem quos nemo nostrum excepturi at explicabo repudiandae
+        voluptatem accusantium.
+      </p>
+      <button>
+        <a href="./login">Login</a>
+      </button>
+      <button>Signup</button>
+      <p>Learn More &darr;</p>
+      <h2>About Us</h2>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi
+        consectetur accusantium sed amet porro, est ipsa dicta? Iste accusamus
+        culpa, dolorem quos nemo nostrum excepturi at explicabo repudiandae
+        voluptatem accusantium.
+      </p>
     </div>
   );
 }
