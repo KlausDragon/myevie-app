8be0d74e967360a2b9ce6cb20a9f25265869ba00
Ali Abbasi <ali_sitrick@yahoo.com>
2024-01-20 16:23:52 -0800

    added classes to myevie

--- evie-client/src/pages/myEvie.js
+++ evie-client/src/pages/myEvie.js
@@ -5,15 +5,15 @@ function MyEvie() {
   return (
     <div className="profile-wrapper">
       <h1>Evie</h1>
-      <p>LVL: 23</p>
+      <p className="lvl">LVL: 23</p>
       <div className="profile picture">
         <img src="" alt="profile picture" />
       </div>
-      <p>Owner: Lorem Ipsum</p>
-      <p>Total Challenges Completed: 1</p>
-      <p>History: </p>
-      <p>2 bottles were recycled on January 1, 2024</p>
-      <p>3 bottles were recycled on December 30, 2023</p>
+      <p className="owner-info">Owner: Lorem Ipsum</p>
+      <p className="total-challenges">Total Challenges Completed: 1</p>
+      <p className="history">History: </p>
+      <p className="first-log">2 bottles were recycled on January 1, 2024</p>
+      <p className="second-log">3 bottles were recycled on December 30, 2023</p>
       <span className="view-history">
         <a>View full history</a>
       </span>
