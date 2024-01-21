e834cbaa6597a77e0a4ce500668b4a265c2e248b
William <ko.william.robert@gmail.com>
2024-01-20 16:22:34 -0800

    Fixed up Login

--- evie-client/src/pages/Login.js
+++ evie-client/src/pages/Login.js
@@ -6,6 +6,11 @@ import Footer from "../components/Footer";
 function Login() {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
+  const [showPassword, setShowPassword] = useState(false);
+
+  const togglePasswordVisibility = () => {
+    setShowPassword(!showPassword);
+  };
 
   const handleLogin = () => {
     // Add your login logic here
@@ -29,10 +34,13 @@ function Login() {
         <label>
           Password:
           <input
-            type="password"
+            type={showPassword ? 'text' : 'password'}
             value={password}
             onChange={(e) => setPassword(e.target.value)}
           />
+          <button type="button" onClick={togglePasswordVisibility} className="toggle-password-button">
+            {showPassword ? 'Hide' : 'Show'} Password
+          </button>
         </label>
         <br />
         <button type="button" onClick={handleLogin} className="login-button">
