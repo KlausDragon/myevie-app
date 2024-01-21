dab5bca241531eacf6d243ea03c8e8846b39a49a
Ali Abbasi <ali_sitrick@yahoo.com>
2024-01-20 16:09:21 -0800

    changed to MyEvie

--- evie-client/src/pages/myEvie.js
+++ evie-client/src/pages/myEvie.js
@@ -1,9 +1,25 @@
-function myEvie() {
+import Nav from "../components/Nav";
+import "../scss/_myevie.scss";
+
+function MyEvie() {
   return (
     <div>
-      <h1>My Evie</h1>
+      <h1>Evie</h1>
+      <p>LVL: 23</p>
+      <div className="profile picture">
+        <img src="https://i.imgur.com/5rJXNl8.png" alt="profile picture" />
+      </div>
+      <p>Owner: Lorem Ipsum</p>
+      <p>Total Challenges Completed: 1</p>
+      <p>History: </p>
+      <p>2 bottles were recycled on January 1, 2024</p>
+      <p>3 bottles were recycled on December 30, 2023</p>
+      <span className="view-history">
+        <a>View full history</a>
+      </span>
+      <Nav />
     </div>
   );
 }
 
-export default myEvie;
+export default MyEvie;
