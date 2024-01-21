e8706b3485e74980b21e899dc41de4b7b56108f2
William <ko.william.robert@gmail.com>
2024-01-21 00:11:42 -0800

    In progress landing page

--- evie-client/src/pages/Landing.js
+++ evie-client/src/pages/Landing.js
@@ -1,35 +1,47 @@
 import React from "react";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
+import "../scss/_landing.scss";
+import Trees from "../media/trees.jpeg";
 
 function Landing() {
   return (
-    <div>
+    <div className="welcome-container">
       <Header />
-      <h1>Welcome to Evie</h1>
+      <h1 className="welcome-label">Welcome to myEvie</h1>
       <p>
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi
         consectetur accusantium sed amet porro, est ipsa dicta? Iste accusamus
         culpa, dolorem quos nemo nostrum excepturi at explicabo repudiandae
         voluptatem accusantium.
       </p>
-      <button>
-        <a href="./login">Login</a>
-      </button>
-      <button>
-        <a href="./signup">Sign Up</a>
-      </button>
-      <button>
-        <a href="./challenges">Challenges</a>
-      </button>
+      <br />
+      <br />
+      <div className="button-container">
+        <button className="green-button">
+          <a href="./login">Login</a>
+        </button>
+        <button className="green-button">
+          <a href="./signup">Sign Up</a>
+        </button>
+        <button className="green-button">
+          <a href="./challenges">Challenges</a>
+        </button>
+      </div>
+      <br />
+      <br />
       <p>Learn More &darr;</p>
+      <br />
+      <br />
       <h2>About Us</h2>
+      <br />
       <p>
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi
         consectetur accusantium sed amet porro, est ipsa dicta? Iste accusamus
         culpa, dolorem quos nemo nostrum excepturi at explicabo repudiandae
         voluptatem accusantium.
       </p>
+      <img src={Trees} alt="My Evie" class="responsive"/>
       <Footer />
     </div>
   );
